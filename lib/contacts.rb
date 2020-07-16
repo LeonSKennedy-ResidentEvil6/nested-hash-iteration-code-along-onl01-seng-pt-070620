99ff98b2b1222d94144d298ce3c5f1a3ae3a2698
Ebo Lee <yiboli.coding@gmail.com>
2020-07-16 16:54:36 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,5 +17,5 @@ require 'pry'
   
 def remove_strawberry(contacts)
   contacts.each do |person,contact_details_hash|
-    contact_details_hash. each do ||
+    contact_details_hash. each do |attribute, detail|
 end
